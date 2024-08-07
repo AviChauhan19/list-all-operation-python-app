23d33b0f3fd08b6aefdbbd78d2dcbd83f0a02c8e
AviChauhan19 <120163723+AviChauhan19@users.noreply.github.com>
2024-08-07 15:32:39 +0530

    added app file

---
+++ day10.py
@@ -0,0 +1,19 @@
+# n=2
+# i=2
+# result=True
+# while i<n:
+#     if n%i==0:
+#         result=False
+#         break
+#     i+=1
+
+# if result:
+#     print("Prime")
+# else:
+#     print("Not Prime")
+
+# x=[1,4,23,78,0,234,1,2,34,67,87,90]
+# l=len(x)
+# d=l//2
+# print(x[0:d])
+# print(x[d:l])
